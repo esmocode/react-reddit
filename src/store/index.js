ddb556d2cbc33836045fa944459c0f80dc927c84
Mohammad <esmodev@gmail.com>
2019-11-29 12:59:15 +0330

    redux devtool

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,13 @@ import thunk from 'redux-thunk';
 import reducers from "./reducers/index";
 import { createStore, applyMiddleware, compose } from "redux";
 
+const composeEnhancers =
+  typeof window === 'object' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?   
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+      // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+    }) : compose;
+
 //Use redux thunk as middleware for async reqs -----
 const middleware = [thunk];
 
@@ -12,7 +19,7 @@ const initState = {
 };
 
 const store = createStore(reducers, initState,
-		applyMiddleware(...middleware),
+	composeEnhancers(applyMiddleware(...middleware)),
 );
 
 export default store;
